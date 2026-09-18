100b73843ae889bae19ded7be94eb7dd1269543e
RevsZs <icf.force0007@gmail.com>
2026-09-17 21:08:35 -0500

    Update moto.java

--- src/moto.java
+++ src/moto.java
@@ -1,1 +1,16 @@
+public class moto extends vehiculos {
 
+    public moto(String marca, String modelo, double tarifa) {
+        super(marca, modelo, tarifa);
+    }
+
+    @Override
+    public double calcularAlquiler(int dias) {
+
+        double recargoFijo = 5;
+
+        double total = (getTarifa() * dias) + recargoFijo;
+
+        return total;
+    }
+}
